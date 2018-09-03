fbc7debc92d231f25e81e54abaad4343bd43ff01
JesseMK <JesseWangZh@hotmail.com>
2018-09-03 18:36:22 -0400

    spec test

--- src/spec_test.py
+++ src/spec_test.py
@@ -1,19 +1,33 @@
-# test
+'''
+tests for coreutils
+'''
+
 import os
 import sys
 
-with open("core_list") as f:
+ori_path = "~/spec_2006/spec_2006_64_bit/"
+res_path = "~/spec_2006/spec_2006_64_bit_reproduce/"
+
+a3_set = {'h264ref_base.i386-m32-gcc42-nn', 'gcc_base.i386-m32-gcc42-nn', 'gobmk_base.i386-m32-gcc42-nn',
+          'perlbench_base.amd64-m64-gcc42-nn', 'gobmk_base.amd64-m64-gcc42-nn', 'gcc_base.amd64-m64-gcc42-nn'}
+a2_set = {'gobmk_base.i386-m32-gcc42-nn',
+          'gobmk_base.amd64-m64-gcc42-nn', 'gcc_base.amd64-m64-gcc42-nn'}
+
+with open("spec_list") as f:
     lines = f.readlines()
 
-# for l in lines[0:1]:
 for l in lines:
-    print '--------------', l.strip(), '------------------------'
-    # os.system("rm " + l.strip())
+    file_name = l.strip()
+    option = ""
+    if file_name in a3_set:
+        option += " -a 3 "
+    if file_name in a2_set:
+        option += " -a 2 "
+
+    print '--------------', file_name, '------------------------'
     sys.stdout.flush()
-    # os.system("rm " + l.strip())
-    # os.system("python uroboros.py /data/ail_data/coreutils_bin_815/" + l.strip())
-    os.system("python uroboros.py ~/spec_2006/spec_2006_64_bit/" + l.strip())
-    # os.system("cp a.out /data/ail_data/coreutils_bin_815_new/" + l.strip())
-    os.system("mv workdir/a.out ~/spec_2006/spec_2006_64_bit_reproduce/" + l.strip())
+    command = "python uroboros.py -o " + res_path + \
+        file_name + " " + ori_path + file_name + option
+    os.system(command)
     print '-----------------------------------------------------'
     sys.stdout.flush()
