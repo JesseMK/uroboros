83bb0e9fbc5e1ab3b3c930f9ec5b153cd349a0c3
JesseMK <JesseWangZh@hotmail.com>
2018-09-04 16:08:09 -0400

    use ori bins

--- test/spec_test.py
+++ test/spec_test.py
@@ -31,6 +31,8 @@ for benchmark in exe_folder:
     all_benchmark += exe_folder[benchmark][4:] + ' '
 
 exe_path = '~/spec_2006/SPEC_CPU2006v1.0/benchspec/CPU2006/{0}/exe/{1}'
+ori_path = '~/spec_2006/SPEC_CPU2006v1.0/benchspec/CPU2006/{0}/exe/{1}.ori'
+rename_exe = 'mv {0} {0}.ori'
 
 a3_set = {'h264ref_base.i386-m32-gcc42-nn', 'gcc_base.i386-m32-gcc42-nn', 'gobmk_base.i386-m32-gcc42-nn',
           'perlbench_base.amd64-m64-gcc42-nn', 'gobmk_base.amd64-m64-gcc42-nn', 'gcc_base.amd64-m64-gcc42-nn'}
@@ -38,13 +40,14 @@ a2_set = {'gobmk_base.i386-m32-gcc42-nn',
           'gobmk_base.amd64-m64-gcc42-nn', 'gcc_base.amd64-m64-gcc42-nn'}
 
 run_uroboros = "python ./../src/uroboros.py -o {0} {1} {2}"
+
+config_spec = 'cd ~/spec_2006/SPEC_CPU2006v1.0 && source ./shrc && cd $OLDPWD'
 # run_spec = 'runspec --config={0} --size=test,train,ref --noreportable --tune=base --iterations=1 {1}'
-run_spec = 'runspec --config={0} --size=test --iterations=1 {1}'
+run_spec = config_spec + \
+    ' && runspec --config={0} --size=test --iterations=1 {1}'
 
 config_x64 = 'test_64.cfg'
 config_x86 = 'test_32.cfg'
-
-os.system('cd ~/spec_2006/SPEC_CPU2006v1.0 && source ./shrc && cd $OLDPWD')
 
 # x64 Loop
 # with open("spec_list_64") as f:
@@ -74,11 +77,11 @@ os.system('cd ~/spec_2006/SPEC_CPU2006v1.0 && source ./shrc && cd $OLDPWD')
 
 # x86 Loop
 with open("spec_list_32") as f:
-    lines=f.readlines()
+    lines = f.readlines()
 
 for l in lines:
-    file_name=l.strip()
-    option=""
+    file_name = l.strip()
+    option = ""
     if file_name in a3_set:
         option += " -a 3"
     if file_name in a2_set:
@@ -86,13 +89,13 @@ for l in lines:
 
     print '--------------', file_name, '------------------------'
     sys.stdout.flush()
-    print run_uroboros.format(exe_path.format(
-        exe_folder[file_name[:-18]], file_name), ori_path_32.format(file_name), option)
-    os.system(run_uroboros.format(exe_path.format(
-        exe_folder[file_name[:-18]], file_name), ori_path_32.format(file_name), option))
+    os.system(rename_exe.format(exe_path.format(
+        exe_folder[file_name[:-18]], file_name)))
+    print run_uroboros.format(exe_path.format(exe_folder[file_name[:-18]], file_name), ori_path.format(exe_folder[file_name[:-18]], file_name), option)
+    os.system(run_uroboros.format(exe_path.format(exe_folder[file_name[
+              :-18]], file_name), ori_path.format(exe_folder[file_name[:-18]], file_name), option))
     print '-----------------------------------------------------'
     sys.stdout.flush()
 
 raw_input(run_spec.format(config_x86, all_benchmark))
 os.system(run_spec.format(config_x86, all_benchmark))
-
