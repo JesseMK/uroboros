b5ed94d8b17a0d94423a87b05e2c4f08b3c7ebff
JesseMK <JesseWangZh@hotmail.com>
2018-09-04 02:23:29 -0400

    debug

--- test/spec_test.py
+++ test/spec_test.py
@@ -39,7 +39,7 @@ run_spec = 'runspec --config={0} --size=test --iterations=1 {1}'
 config_x64 = 'test_64.cfg'
 config_x86 = 'test_32.cfg'
 
-os.system('cd ~/spec_2006/SPEC_CPU2006v1.0 && source shrc && cd $OLDPWD')
+os.system('cd ~/spec_2006/SPEC_CPU2006v1.0 && source ./shrc && cd $OLDPWD')
 
 # x64 Loop
 with open("spec_list_64") as f:
@@ -59,11 +59,11 @@ for l in lines:
         exe_folder[file_name[:-19]], file_name), ori_path_64.format(file_name), option)
     os.system(run_uroboros.format(exe_path.format(
         exe_folder[file_name[:-19]], file_name), ori_path_64.format(file_name), option))
+    raw_input(run_spec.format(config_x64, file_name[:-24]))
+    os.system(run_spec.format(config_x64, file_name[:-24]))
     print '-----------------------------------------------------'
     sys.stdout.flush()
 
-raw_input(run_spec.format(config_x64, file_name[:-19]))
-os.system(run_spec.format(config_x64, file_name[:-19]))
 
 raw_input()
 
@@ -85,8 +85,8 @@ for l in lines:
         exe_folder[file_name[:-19]], file_name), ori_path_32.format(file_name), option)
     os.system(run_uroboros.format(exe_path.format(
         exe_folder[file_name[:-19]], file_name), ori_path_32.format(file_name), option))
+    raw_input(run_spec.format(config_x86, file_name[:-19]))
+    os.system(run_spec.format(config_x86, file_name[:-19]))
     print '-----------------------------------------------------'
     sys.stdout.flush()
 
-raw_input(run_spec.format(config_x86, file_name[:-19]))
-os.system(run_spec.format(config_x86, file_name[:-19]))
